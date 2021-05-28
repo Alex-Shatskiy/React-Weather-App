4f52cee16b0b3390dcf0a0bde8ac6a0be6f5e38b
Alex-Shatskiy <alexlev706@gmail.com>
2021-05-28 14:35:58 +1200

    react weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import ReactDOM from "react-dom"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const api = {
+  apiKey: "c1477f7c454a4e7e7dfd812a8b87d88d",
+  base: "https://api.openweathermap.org/data/2.5/weather?q=",
 }
 
-export default App;
+function App() {
+  const [query, setQuery] = useState("")
+  const [weather, setWeather] = useState({})
+
+  const search = (evt) => {
+    if (evt.key == "Enter") {
+      fetch(`${api.base}${query}&units=metric&appid=${api.apiKey}`)
+        .then((res) => res.json())
+        .then((result) => {
+          console.log(result)
+          setQuery("")
+          setWeather(result)
+        })
+    }
+  }
+
+  const getDate = (dates) => {
+    const months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ]
+    const days = [
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday",
+    ]
+    let day = days[dates.getDay() - 1]
+    let date = dates.getDate()
+    let month = months[dates.getMonth()]
+    let year = dates.getFullYear()
+
+    return `${day} ${date} ${month} ${year}`
+  }
+
+  return (
+    <div
+      className={
+        typeof weather.main != "undefined"
+          ? weather.main.temp > 12
+            ? "app"
+            : "app cold"
+          : "app"
+      }
+    >
+      <main>
+        <div className="search-box">
+          <input
+            className="search-bar"
+            type="text"
+            placeholder="Search..."
+            onChange={(evt) => setQuery(evt.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {typeof weather.main != "undefined" ? (
+          <>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{getDate(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp"> {Math.round(weather.main.temp)}°C</div>
+              <div className="type">{weather.weather[0].main}</div>
+            </div>
+          </>
+        ) : (
+          <></>
+        )}
+      </main>
+    </div>
+  )
+}
+
+export default App
